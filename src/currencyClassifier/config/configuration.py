2d711a1109f7ea80a02eecf031f0aec8597c0f36
Vedansh1857 <vedanshgupta606@gmail.com>
2024-08-28 18:57:21 +0530

    prepare base model added

--- src/currencyClassifier/config/configuration.py
+++ src/currencyClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from currencyClassifier.constants import *
 from currencyClassifier.utils.common import read_yaml, create_directories
-from currencyClassifier.entity.config_entity import DataIngestionConfig
+from currencyClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig)
 
 class ConfigurationManager():
     def __init__(self,config_filepath = CONFIG_FILE_PATH,params_filepath=PARAMS_FILE_PATH):
@@ -24,3 +24,22 @@ class ConfigurationManager():
         )
 
         return data_ingestion_config
+    
+
+    def get_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_classes=self.params.CLASSES,
+            params_weights=self.params.WEIGHTS,
+            params_learning_rate=self.params.LEARNING_RATE
+        )
+
+        return base_model_config
