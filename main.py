7d353919484d5dba2f687af99884f7b9a650e999
Vedansh1857 <vedanshgupta606@gmail.com>
2024-08-30 22:52:01 +0530

    Model training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.currencyClassifier import logger
 from src.currencyClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.currencyClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.currencyClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -17,6 +18,17 @@ STAGE_NAME = "Prepare Base Model Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model training Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/currencyClassifier/config/configuration.py
+++ src/currencyClassifier/config/configuration.py
@@ -1,6 +1,7 @@
+import os
 from currencyClassifier.constants import *
 from currencyClassifier.utils.common import read_yaml, create_directories
-from currencyClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig)
+from currencyClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, ModelTrainingConfig)
 
 class ConfigurationManager():
     def __init__(self,config_filepath = CONFIG_FILE_PATH,params_filepath=PARAMS_FILE_PATH):
@@ -43,3 +44,25 @@ class ConfigurationManager():
         )
 
         return base_model_config
+    
+
+    def get_model_training_config(self) -> ModelTrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir,"Currency_image")
+
+        create_directories([Path(training.root_dir)])
+
+        model_training_config = ModelTrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return model_training_config
